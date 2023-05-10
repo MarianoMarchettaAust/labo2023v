4f2a1b18dc912b9cb19fd8a4255543e7cf81222a
Mariano Marchetta <mmarchetta@mulesoft.com>
2023-05-10 08:58:35 -0300

    chore: add custom scripts

--- src/colaborativos-default/z651_HT_lightgbm.r
+++ src/colaborativos-default/z651_HT_lightgbm.r
@@ -31,13 +31,13 @@ options(error = function() {
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento <- "HT6510"
+PARAM$experimento <- "HT6510_X"
 
-PARAM$exp_input  <- "TS6410"
+PARAM$exp_input  <- "TS6410_X"
 
 PARAM$lgb_crossvalidation_folds  <- 5  #En caso que se haga cross validation, se usa esta cantidad de folds
 
-PARAM$lgb_semilla  <- 102191   #cambiar por su propia semilla
+PARAM$lgb_semilla  <- 100019   #cambiar por su propia semilla
 
 
 #Hiperparametros FIJOS de  lightgbm
